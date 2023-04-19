05d08d996cfbfd4a64ee09ce6c34104c6784bbd0
Jake Bogan <jakebogan01@gmail.com>
2023-04-19 02:03:04 -0400

    adds basic html foundation for dashboard page

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,3 +1,7 @@
-<div>
-     Dashboard Page
+<div class="flex">
+     <div class="border-2 border-red-500"></div>
+     <div class="flex flex-col border-2 border-orange-500">
+          <div class="border-2 border-blue-500"></div>
+          <div class="border-2 border-green-500"></div>
+     </div>
 </div>

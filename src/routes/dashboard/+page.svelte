f052dec40515e0e68da4ee97ddcceeea8f0e95c0
Jake Bogan <jakebogan01@gmail.com>
2023-04-21 03:00:52 -0400

    fixes dashboard active tab

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -141,7 +141,7 @@
                <div class="mt-5">
                     <ul>
                          {#each $BoardStore as board (board?.id)}
-                              <li class="flex items-center space-x-4 pl-8 mr-6 py-[0.8rem] font-bold text-[0.9375rem] rounded-tr-full rounded-br-full cursor-pointer {active ? " bg-[#635FC7] text-white" : ""}" on:click={() => findBoard(board?.id, board?.title)} on:keydown={()=>{}}>
+                              <li class="flex items-center space-x-4 pl-8 mr-6 py-[0.8rem] font-bold text-[0.9375rem] rounded-tr-full rounded-br-full cursor-pointer {(board?.id === specificId) ? " bg-[#635FC7] text-white" : ""}" on:click={() => findBoard(board?.id, board?.title)} on:keydown={()=>{}}>
                                    <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M0 2.889A2.889 2.889 0 0 1 2.889 0H13.11A2.889 2.889 0 0 1 16 2.889V13.11A2.888 2.888 0 0 1 13.111 16H2.89A2.889 2.889 0 0 1 0 13.111V2.89Zm1.333 5.555v4.667c0 .859.697 1.556 1.556 1.556h6.889V8.444H1.333Zm8.445-1.333V1.333h-6.89A1.556 1.556 0 0 0 1.334 2.89V7.11h8.445Zm4.889-1.333H11.11v4.444h3.556V5.778Zm0 5.778H11.11v3.11h2a1.556 1.556 0 0 0 1.556-1.555v-1.555Zm0-7.112V2.89a1.555 1.555 0 0 0-1.556-1.556h-2v3.111h3.556Z" fill="currentColor"/></svg>
                                    <span>{board?.title}</span>
                               </li>
